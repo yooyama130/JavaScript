d53ef9b8c6b3ee0fc0e4b279ca4dd26008be3481
unknown <92510176+yooyama130@users.noreply.github.com>
2021-11-23 10:19:56 +0900

    [Finish] 2章

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-alert('Hello World');
+// 条件分岐
+let orange = 100;
+let apple = 120;
+
+if (orange < apple) {
+  alert('みかんの値段がりんごより安い');
+} else if (orange == apple) {
+  alert('みかんとりんごが同じ値段');
+} else {
+  alert('みかんの値段がりんごより高い');
+}
+
+// 繰り返し（while)
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num <max){
+  num = num*2;
+  count = count+1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// 繰り返し(for)
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
